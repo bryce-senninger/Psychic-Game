b1736449ca7f56e3d74d6747889feeea350f4aa1
Bryce <bryce.senninger@gmail.com>
2019-05-30 13:23:34 -0600

    finished game!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,75 @@
 let computerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let computerGuess = computerArray[Math.floor(Math.random()*computerArray.length)];
-console.log(computerGuess);
 
-function chooseRandom () {
-    let computerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let wins = 0;
+
+let losses = 0;
+
+let remaining = 9;
+
+let guesses = []
+
+function updateWins() {
+    document.getElementById("wins").innerHTML = "Wins: " + wins;
+}
+
+function updateLoss() {
+    document.getElementById("losses").innerHTML = "Losses: " + losses; 
+}
+
+function updateRemaining() {
+    document.getElementById("remaining").innerHTML = "Remaining Guesses: " + remaining;
+}
+
+function updateGuesses() {
+    document.getElementById("guessed").innerHTML = "Guessed Letters: " + guesses;
+}
+
+function reset() {
+    guesses = [];
+    remaining = 9;
+    updateGuesses();
+    updateRemaining();
+}
+
+function chooseRandom() {
     let computerGuess = computerArray[Math.floor(Math.random()*computerArray.length)];
     console.log(computerGuess);
-}
-
-let wins = 0
-
-let losses = 0
-
-
-// console.log(computerGuess); //this is not necessary, just to help me see if it's working
- 
-
-function updateScore() {
-    //write function here to call in if statement below
-
-}
-
-document.onkeyup = function(event) {
-    let letter = event.key.toLowerCase();
+    document.onkeyup = function(event) {
+        let letter = event.key.toLowerCase();
         if (letter === computerGuess) {
-            alert("WIN");
-            // call appropriate update score function
+            wins ++;
+            updateWins();
             chooseRandom();
+        } else {
+            remaining --;
+            guesses.push(" " + letter);
+            updateGuesses();
+            updateRemaining();
+            function gameEnd() {
+                if (remaining === 0) {
+                    losses ++;
+                    updateLoss();
+                    alert("It was " + computerGuess + "!")
+                    reset();
+                    chooseRandom();
+                }
+            }
+            gameEnd();
         }
+    }
 
 }
 
+chooseRandom();
 
+
+
+function playAudio() {
+    let music = document.getElementById("myAudio");
+    music.play();
+}
+
+function pauseAudio() {
+    let music = document.getElementById("myAudio");
+    music.pause();
+}
